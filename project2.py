b755a5056dc4a1dd0aa2251a42f539c53a70ea17
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-08-11 13:45:14 +0530

    cloning phonephe repository

---
+++ project2.py
@@ -0,0 +1,8 @@
+import git
+
+url = "https://github.com/PhonePe/pulse.git"
+
+folder = "Phonepe-Pulse-Data-Visualization-and-Exploration_-A-User-Friendly-Tool-Using-Streamlit-and-Plotly"
+
+git.Repo.clone_from(url,folder)
+
