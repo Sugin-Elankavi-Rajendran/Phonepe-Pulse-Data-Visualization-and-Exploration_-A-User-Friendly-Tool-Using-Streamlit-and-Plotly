902da3e7e5b1f591e711448d3e2d735b7e7e1895
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-08-22 14:10:59 +0530

    Gave options for the user

--- Phonepe.py
+++ Phonepe.py
@@ -21,3 +21,6 @@ st.write("The following data is from the year 2018-2022:")
 
 ##################
 
+option = st.radio("Select anyone of the following option:",('All India', 'State wise','Top Ten categories'),horizontal=True)
+
+##################
